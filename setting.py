a6a0cac9cb6732029d1c67e5c582c68b2e34a25c
Полина Третьякова <129007532+PolinaTretyackova@users.noreply.github.com>
2023-05-06 21:19:02 +0300

    setting.py

--- setting.py
+++ setting.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-valid_email = os.getenv('valid_email')
-valid_password = os.getenv('valid_password')
+valid_email = polinaTretyackova.@mail.ru('valid_email')
+valid_password = 12345('valid_password')
 
 invalid_email = '123@yahoo.com'
 invalid_password = '225588'
